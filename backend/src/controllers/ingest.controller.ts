285d1b80b2a39a207c7e55319440e05ccfa13271
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2026-01-15 18:00:59 +0530

    add pdf parsing logic

--- backend/src/controllers/ingest.controller.ts
+++ backend/src/controllers/ingest.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { processText } from "../utils/textProcessor";
+import { PDFParse } from "pdf-parse";
 
 export const ingestText = async (req: Request, res: Response) => {
   try {
@@ -23,5 +24,35 @@ export const ingestText = async (req: Request, res: Response) => {
   } catch (error) {
     console.error("Ingestion error:", error);
     res.status(500).json({ error: "Failed to ingest text." });
+  }
+};
+
+export const ingestPDF = async (req: Request, res: Response) => {
+  try {
+    const { sessionId } = req.body;
+    const file = req.file;
+
+    if (!file || !sessionId) {
+      return res.status(400).json({ error: "Missing PDF file or sessionId" });
+    }
+
+    // convert buffer to Uint8Array
+    const pdfData = new PDFParse(new Uint8Array(file.buffer));
+    const text = (await pdfData.getText()).text;
+
+    const result = await processText(text, {
+      summarize: true,
+      embed: true,
+      sessionId,
+    });
+
+    return res.status(200).json({
+      message: "PDF ingested successfully",
+      summary: result.summary,
+      chunkCount: result.chunkCount,
+    });
+  } catch (error) {
+    console.error("PDF Ingestion error:", error);
+    res.status(500).json({ error: "Failed to ingest PDF." });
   }
 };
--- backend/src/routes/ingest.route.ts
+++ backend/src/routes/ingest.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { ingestText } from "../controllers/ingest.controller";
+import { ingestText, ingestPDF } from "../controllers/ingest.controller";
+import upload from "../config/multer";
 
 const router = Router();
 
 router.post("/ingest-text", ingestText);
+router.post("/ingest-pdf", upload.single("file"), ingestPDF);
 
 export default router;
